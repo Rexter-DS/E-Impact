762473d0389939a02f4dd1ad92a5282cc8f5effe
nicholasmp <nkmp@hawaii.edu>
2021-02-04 13:32:28 -1000

    Update QuickAccess.jsx

--- app/imports/ui/pages/QuickAccess.jsx
+++ app/imports/ui/pages/QuickAccess.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 // import { Grid, Image } from 'semantic-ui-react';
 
-const QuickAccessForm = () => (
+const QuickAccessInput = () => (
       <form className='quick-access-form'>
-        <div>
-          <label>Miles Traveled</label>
+        <div className='form-control'>
+          <label>Distance Traveled(mi)</label>
           <input type='text' placeholder='0'/>
         </div>
+        <div className='form-control'>
+          <label>Mode of Transportation</label>
+          <input type='text' placeholder='0'/>
+        </div>
+        <div className='form-control'>
+          <label>Vehicle Miles per Gallon(gal)</label>
+          <input type='text' placeholder='0'/>
+        </div>
+        <input type='submit' value='Submit'/>
       </form>
   );
-export default QuickAccessForm;
+export default QuickAccessInput;
