4bf946bb7ef2671dc90f5625c6e564b6296485bb
nicholasmp <nkmp@hawaii.edu>
2021-02-04 12:27:04 -1000

    Create QuickAccess.jsx

---
+++ app/imports/ui/pages/QuickAccess.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Grid, Image } from 'semantic-ui-react';
+
+const QuickAccessForm = () => (
+      <form className='quick-access-form'>
+        <div>
+          <label>Miles Traveled</label>
+          <input type='text' placeholder='0'/>
+        </div>
+      </form>
+  );
+export default QuickAccessForm;
