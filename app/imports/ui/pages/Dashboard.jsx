84c36ec712c84737d0a1c85e4103bd903745a4fd
Rexter Delos Santos <rexterds@hawaii.edu>
2021-02-10 17:49:11 -1000

    Added appropriate navbar
    Waiting for sidebar to be finished and pushed to main

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Menu } from 'semantic-ui-react';
 import { ResponsiveContainer, LineChart, Line, PieChart, Pie, BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import NavBar from '../components/NavBar';
 
 /** A simple static component to render some text for the landing page. */
 const data = [
@@ -53,62 +54,66 @@ class Dashboard extends React.Component {
 
   handleItemClick = (e, { name }) => {
     this.setState({ activeItem: name });
-
   };
 
   render() {
     const { activeItem } = this.state;
 
     return (
-        <Grid id='dashboard-page' columns={2} padded="vertically" container>
-          <Grid.Row>
-            <Grid.Column>
-              <Grid.Row>
-                <Menu fluid horizontal>
-                  <Menu.Item
-                      name='Today'
-                      active={activeItem === 'Today'}
-                      onClick={this.handleItemClick}
-                  />
-                </Menu>
-              </Grid.Row>
-              <Grid.Row>
+        <div id="dashboard-container">
+
+          <NavBar/>
+
+          { /* Contains the graphs that dislays the data */ }
+          <Grid id='dashboard' columns={2} padded="vertically" verticalAlign='middle' container>
+            <Grid.Row>
+              <Grid.Column>
+                <Grid.Row>
+                  <Menu fluid horizontal>
+                    <Menu.Item
+                        name='Today'
+                        active={activeItem === 'Today'}
+                        onClick={this.handleItemClick}
+                    />
+                  </Menu>
+                </Grid.Row>
+                <Grid.Row>
+                  <ResponsiveContainer width='100%' height={250}>
+                    <BarChart data={data}>
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" tick={{ fill: 'white' }}/>
+                      <YAxis tick={{ fill: 'white' }}/>
+                      <Tooltip />
+                      <Legend />
+                      <Bar dataKey="pv" fill="#8884d8" />
+                      <Bar dataKey="uv" fill="#82ca9d" />
+                    </BarChart>
+                  </ResponsiveContainer>;
+                </Grid.Row>
+              </Grid.Column>
+              <Grid.Column>
                 <ResponsiveContainer width='100%' height={250}>
                   <BarChart data={data}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
+                    <XAxis dataKey="name" tick={{ fill: 'white' }}/>
+                    <YAxis tick={{ fill: 'white' }}/>
                     <Tooltip />
                     <Legend />
                     <Bar dataKey="pv" fill="#8884d8" />
                     <Bar dataKey="uv" fill="#82ca9d" />
                   </BarChart>
-                </ResponsiveContainer>;
-              </Grid.Row>
-            </Grid.Column>
-            <Grid.Column>
-              <ResponsiveContainer width='100%' height={250}>
-                <BarChart data={data}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend />
-                  <Bar dataKey="pv" fill="#8884d8" />
-                  <Bar dataKey="uv" fill="#82ca9d" />
-                </BarChart>
-              </ResponsiveContainer>
-            </Grid.Column>
-          </Grid.Row>
+                </ResponsiveContainer>
+              </Grid.Column>
+            </Grid.Row>
 
-          <Grid.Row>
+            <Grid.Row>
               <Grid.Column>
                 <ResponsiveContainer width='100%' height={250}>
                   <LineChart data={data}
                              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray='3 3' />
-                    <XAxis dataKey='name' />
-                    <YAxis />
+                    <XAxis dataKey='name' tick={{ fill: 'white' }}/>
+                    <YAxis tick={{ fill: 'white' }}/>
                     <Tooltip />
                     <Legend />
                     <Line type='monotone' dataKey='pv' stroke='#8884d8' />
@@ -123,10 +128,10 @@ class Dashboard extends React.Component {
                   </PieChart>
                 </ResponsiveContainer>
               </Grid.Column>
-          </Grid.Row>
-        </Grid>
+            </Grid.Row>
+          </Grid>
+        </div>
     );
   }
 }
-
 export default Dashboard;
