e2b6e952b61adf16ec163cd999b5daa7502c243f
Rexter Delos Santos <rexterds@hawaii.edu>
2021-02-12 11:36:33 -1000

    updated layout for quick access

--- app/imports/ui/pages/QuickAccess.jsx
+++ app/imports/ui/pages/QuickAccess.jsx
@@ -1,52 +1,44 @@
 import React, { useState } from 'react';
-import { Grid } from 'semantic-ui-react';
-import qaLogo from '../../../public/images/QuickAccessLogo.png';
+import { Grid, Form, Input, Select } from 'semantic-ui-react';
 
 function QuickAccess() {
   const [miles, setMiles] = useState('');
   const [mpg, setMpg] = useState('');
   const [ghg, setGhg] = useState(1);
   return (
-      <div className="base">
-        <form className="main">
-          <img src={qaLogo} height={102} width={271} alt="Quick Access"/>
-          <Grid columns={2} rows={3}>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <label>Distance traveled(mi)</label>
-              </Grid.Column>
-              <Grid.Column>
-                <input type="text" value={miles} placeholder="0" onChange={e => setMiles(e.target.value)}/>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <label>Mode of Transportation</label>
-              </Grid.Column>
-              <Grid.Column>
-                <select>
-                  <option>Bike</option>
-                  <option>Bus</option>
-                  <option>Car</option>
-                  <option>Foot</option>
-                </select>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <label>Vehicle Miles Per Gallon</label>
-              </Grid.Column>
-              <Grid.Column>
-                <form>
-                  <input type="text" value={mpg} placeholder='0' onChange={e => setMpg(e.target.value)}/>
-                </form>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </form>
-        <p>Miles Traveled: {miles}; Mileage Of Vehicle(if used): {mpg}</p>
-        <label onInput={() => setGhg((miles / mpg) * 8887)}>You produced {ghg} grams of CO2/gallon. </label>
-      </div>
+      <Grid id="quick-access-container" centered>
+        <Grid.Row>
+          <img src={'/images/QuickAccessLogo.png'} height={102} width={271} alt="Quick Access"/>
+        </Grid.Row>
+        <Grid.Row>
+          <Form>
+            <Form.Field
+                control={Input}
+                label="Distance traveled (mi)"
+                onChange={e => setMiles(e.target.value)}
+            />
+            <Form.Field
+                control={Select}
+                label="Mode of transportation"
+                options={[
+                  { text: 'Bike', value: 'bike' },
+                  { text: 'Bus', value: 'bus' },
+                  { text: 'Car', value: 'car' },
+                  { text: 'Foot', value: 'foot' },
+                ]}
+            />
+            <Form.Field
+                control={Input}
+                label="Vehicle miles per gallon"
+                onChange={e => setMpg(e.target.value)}
+            />
+          </Form>
+        </Grid.Row>
+        <Grid.Row>
+          <p>Miles Traveled: {miles}; Mileage Of Vehicle(if used): {mpg}</p>
+          <label onInput={() => setGhg((miles / mpg) * 8887)}>You produced {ghg} grams of CO2/gallon. </label>
+        </Grid.Row>
+      </Grid>
   );
 }
 export default QuickAccess;
