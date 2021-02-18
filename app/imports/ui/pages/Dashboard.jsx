938be75c538841f31a1802dcd79c1e96f62e969c
Rexter Delos Santos <rexterds@hawaii.edu>
2021-02-17 23:38:53 -1000

    Synced data to the dashboard
    -currently using Person 4's data
    -Meteor.call may be causing error. Crashes after 10 min of no meteor server restart. Need to look into it more.
    "FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory"

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,41 +1,8 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Menu, Image, Container, Header } from 'semantic-ui-react';
+import { Grid, Container, Header } from 'semantic-ui-react';
 import { ResponsiveContainer, LineChart, Line, PieChart, Pie, BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import SidebarVisible from '../components/SideBar';
-
-function BarGraph(data) {
-  return (
-      <ResponsiveContainer width='100%' height={250}>
-        <BarChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" tick={{ fill: 'white' }}/>
-          <YAxis tick={{ fill: 'white' }}/>
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
-  );
-}
-
-function LineGraph(data) {
-  return (
-      <ResponsiveContainer width='100%' height={250}>
-        <LineChart data={data}
-                   margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='name' tick={{ fill: 'white' }}/>
-          <YAxis tick={{ fill: 'white' }}/>
-          <Tooltip />
-          <Legend />
-          <Line type='monotone' dataKey='pv' stroke='#8884d8' />
-          <Line type='monotone' dataKey='uv' stroke='#82ca9d' />
-        </LineChart>
-      </ResponsiveContainer>
-  );
-}
 
 const customizedPieGraphLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
@@ -47,17 +14,7 @@ const customizedPieGraphLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, p
         {`${(percent * 100).toFixed(0)}%`}
       </text>
   );
-}
-
-function PieGraph(props) {
-  return (
-      <ResponsiveContainer>
-        <PieChart>
-          <Pie data={props} cx="50%" cy="50%" label={customizedPieGraphLabel} outerRadius={100} fill="#8884d8" />
-        </PieChart>
-      </ResponsiveContainer>
-  );
-}
+};
 
 function Overall() {
   const [totalMiles, setTotalMiles] = useState(0);
@@ -90,24 +47,51 @@ function Overall() {
   });
 
   return (
-      <Grid id="overall-container" stretched container padding="vertically" columns={3}>
-        <Grid.Column>
-          <Grid.Row>
-            <Container text>
+      <Grid id="overall-container" columns={3}>
+        <Grid.Column id="overall-left-grid">
+          <Grid.Row id="overall-text">
+            <Container fluid text>
               <Header>Overall</Header>
             </Container>
           </Grid.Row>
-          <Grid.Row>
-            { totalMiles } Total Miles
+          <Grid.Row id="overall-total-miles">
+            <Grid.Row>
+              <Container fluid text style={{ fontSize: '50px' }} textAlign="center">
+                { totalMiles }
+              </Container>
+            </Grid.Row>
+            <Grid.Row>
+              <Container fluid text textAlign="center">
+                Total Miles
+              </Container>
+            </Grid.Row>
           </Grid.Row>
         </Grid.Column>
 
-        <Grid.Column>
+        <Grid.Column stretched>
           <Grid.Row>
-            { fuelSaved } of fuel saved
+            <Grid.Row>
+              <Container fluid text style={{ fontSize: '50px' }} textAlign="center">
+                { fuelSaved } lbs
+              </Container>
+            </Grid.Row>
+            <Grid.Row>
+              <Container fluid text textAlign="center">
+                of fuel saved
+              </Container>
+            </Grid.Row>
           </Grid.Row>
           <Grid.Row>
-            { ghgReduced } of GHG reduced
+            <Grid.Row>
+              <Container fluid text style={{ fontSize: '50px' }} textAlign="center">
+                { ghgReduced } lbs
+              </Container>
+            </Grid.Row>
+            <Grid.Row>
+              <Container fluid text textAlign="center">
+                of GHG reduced
+              </Container>
+            </Grid.Row>
           </Grid.Row>
         </Grid.Column>
 
@@ -121,23 +105,85 @@ function Overall() {
       </Grid>
   );
 }
-/* The dashboard that contains graphs that contains the graphs to display data to the user */
-function Dashboard() {
 
-  Meteor.call('getData', function (error, result) {
-    if (error) {
-      console.log(error.reason);
-    }
-    if (result) {
-      console.log(result);
+function MilesSavedPerDay() {
+  const [milesSaved, setMilesSaved] = useState([]);
+
+  Meteor.call('getMilesSavedPerDay', function (error, result) {
+    if (!error) {
+      setMilesSaved(result);
     }
   });
 
-  Meteor.call('getModesOfTransport');
+  return (
+      <Grid id="miles-saved-container" column={1}>
+        <Grid.Column>
+          <Grid.Row id="miles-saved-header">
+            <Container id="miles-saved-header-container" fluid text>
+              <Header>Miles saved per day</Header>
+            </Container>
+          </Grid.Row>
+          <Grid.Row id="miles-saved-graph">
+            <ResponsiveContainer width='100%' height={250}>
+              <BarChart data={milesSaved}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="day" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="miles" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
+  );
+}
+
+function MonthlyGHGReport() {
+  const [monthlyReport, setMonthlyReport] = useState([]);
+
+  Meteor.call('getMonthlyGHGReport', function (error, result) {
+    if (!error) {
+      setMonthlyReport(result);
+    }
+  });
+
+  return (
+      <Grid id="monthly-report-container" columns={1}>
+        <Grid.Column>
+          <Grid.Row id="monthly-report-header">
+            <Container id="monthly-report-header-container" fluid text>
+              <Header>Monthly GHG report</Header>
+            </Container>
+          </Grid.Row>
+          <Grid.Row id="monthly-report-graph">
+            <ResponsiveContainer width='100%' height={250}>
+              <LineChart data={monthlyReport}
+                         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis dataKey='month'/>
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type='monotone' dataKey='GHGReduced' stroke='#8884d8' />
+              </LineChart>
+            </ResponsiveContainer>
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
+  );
+}
+/* The dashboard that contains graphs that contains the graphs to display data to the user */
+function Dashboard() {
+
+  Meteor.call('getMonthlyGHGReport');
   return (
       <div id="dashboard-container">
         <SidebarVisible/>
         <Overall/>
+        <MilesSavedPerDay/>
+        <MonthlyGHGReport/>
       </div>
   );
 }
--- app/server/methods.js
+++ app/server/methods.js
@@ -10,7 +10,7 @@ Meteor.methods({
 
     let totalMiles = 0;
     let numberOfTrips = 0;
-    data[0].trips.forEach((trip) => {
+    data[3].trips.forEach((trip) => {
       switch (trip.modeOfTransportation) {
         default:
           break;
@@ -24,23 +24,60 @@ Meteor.methods({
           break;
       }
     });
-    totalMiles = numberOfTrips * data[0].homeRoundTrip;
+    totalMiles = numberOfTrips * data[3].homeRoundTrip;
 
-    return totalMiles;
+    return totalMiles.toFixed(2);
   },
   getFuelSaved: function () {
     const data = JSON.parse(Assets.getText('data.json'));
 
-    const totalMiles = Meteor.call('getMilesTotal');
-    const fuelSaved = totalMiles / data[0].autoMPG;
+    let totalMiles = 0;
+    let numberOfTrips = 0;
+    data[3].trips.forEach((trip) => {
+      switch (trip.modeOfTransportation) {
+        default:
+          break;
+        case 'T':
+        case 'P':
+        case 'B':
+        case 'W':
+        case 'C':
+        case 'E':
+          numberOfTrips += 1;
+          break;
+      }
+    });
 
-    return fuelSaved;
+    totalMiles = numberOfTrips * data[3].homeRoundTrip;
+    const fuelSaved = totalMiles / data[3].autoMPG;
+
+    return fuelSaved.toFixed(2);
   },
   getGHGReduced: function () {
-    const fuelSaved = Meteor.call('getFuelSaved');
+    const data = JSON.parse(Assets.getText('data.json'));
+
+    let totalMiles = 0;
+    let numberOfTrips = 0;
+    data[3].trips.forEach((trip) => {
+      switch (trip.modeOfTransportation) {
+        default:
+          break;
+        case 'T':
+        case 'P':
+        case 'B':
+        case 'W':
+        case 'C':
+        case 'E':
+          numberOfTrips += 1;
+          break;
+      }
+    });
+    totalMiles = numberOfTrips * data[3].homeRoundTrip;
+
+    const fuelSaved = totalMiles / data[3].autoMPG;
     const ghgReduced = fuelSaved * 19.6;
 
-    return ghgReduced;
+    return ghgReduced.toFixed(2);
   },
   getModesOfTransport: function () {
     const data = JSON.parse(Assets.getText('data.json'));
@@ -54,7 +91,7 @@ Meteor.methods({
       { mode: 'Electric Vehicle', value: 0 },
     ];
 
-    data[0].trips.forEach((trip) => {
+    data[3].trips.forEach((trip) => {
       switch (trip.modeOfTransportation) {
         default:
           break;
@@ -81,4 +118,98 @@ Meteor.methods({
 
     return modes;
   },
+  getMilesSavedPerDay: function () {
+    const data = JSON.parse(Assets.getText('data.json'));
+
+    const milesSaved = [
+      { day: 'Sunday', miles: 0 },
+      { day: 'Monday', miles: 0 },
+      { day: 'Tuesday', miles: 0 },
+      { day: 'Wednesday', miles: 0 },
+      { day: 'Thursday', miles: 0 },
+      { day: 'Friday', miles: 0 },
+      { day: 'Saturday', miles: 0 },
+    ];
+
+    let index = 0;
+    data[3].trips.forEach((trip) => {
+      if (trip.modeOfTransportation !== '') {
+        switch (index) {
+          default:
+            index = 0;
+            break;
+          case 0:
+            milesSaved[0].miles += data[3].homeRoundTrip;
+            break;
+          case 1:
+            milesSaved[1].miles += data[3].homeRoundTrip;
+            break;
+          case 2:
+            milesSaved[2].miles += data[3].homeRoundTrip;
+            break;
+          case 3:
+            milesSaved[3].miles += data[3].homeRoundTrip;
+            break;
+          case 4:
+            milesSaved[4].miles += data[3].homeRoundTrip;
+            break;
+          case 5:
+            milesSaved[5].miles += data[3].homeRoundTrip;
+            break;
+          case 6:
+            milesSaved[6].miles += data[3].homeRoundTrip;
+            break;
+        }
+      }
+      index += 1;
+    });
+
+    return milesSaved;
+  },
+  getMonthlyGHGReport: function () {
+    const data = JSON.parse(Assets.getText('data.json'));
+
+    let month;
+
+    const monthlyReport = [
+      { month: 'March', GHGReduced: 0 },
+      { month: 'April', GHGReduced: 0 },
+    ];
+
+    data[3].trips.forEach((trip) => {
+      switch (trip.modeOfTransportation) {
+        default:
+          break;
+        case 'T':
+        case 'P':
+        case 'B':
+        case 'W':
+        case 'C':
+        case 'E':
+          month = trip.date.split("-");
+          switch (month[1]) {
+            default:
+              break;
+            case '03':
+              monthlyReport[0].GHGReduced += (data[3].homeRoundTrip / data[3].autoMPG) * 19.6;
+
+              break;
+            case '04':
+              monthlyReport[1].GHGReduced += (data[3].homeRoundTrip / data[3].autoMPG) * 19.6;
+              break;
+          }
+          break;
+      }
+    });
+
+    let temp;
+
+    temp = monthlyReport[0].GHGReduced;
+    monthlyReport[0].GHGReduced = temp.toFixed(2);
+
+    temp = monthlyReport[1].GHGReduced;
+    monthlyReport[1].GHGReduced = temp.toFixed(2);
+
+    return monthlyReport;
+  }
 });
