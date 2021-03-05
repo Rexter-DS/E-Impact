eb3fe461011c9be8eb813bbd8dfd5417c2a8ff6f
Jake Camarao <jcamarao@hawaii.edu>
2021-03-05 09:24:06 -1000

    states exist now

--- app/imports/ui/components/Map.jsx
+++ app/imports/ui/components/Map.jsx
@@ -3,39 +3,98 @@ import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 const mapStyles = {
   height: '400px',
-  width: '89.6%',
+  width: '90%',
 };
 
 export class MapContainer extends React.Component {
   state = {
     showingInfoWindow: false,
+    hawaiiCounty: false,
+    honoluluCounty: false,
+    mauiCounty: false,
+    kauaiCounty: false,
+    kalawaoCounty: false,
     activeMarker: {},
     selectedPlace: {},
   };
 
-  onMarkerClick = (props, marker, e) => this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true,
-      });
+  // onMarkerClick = (props, marker, e) => this.setState({
+  //       selectedPlace: props,
+  //       activeMarker: marker,
+  //       showingInfoWindow: true,
+  //     });
 
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
-    }
-  };
+  onHonoluluClick = (props, marker) => this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    honoluluCounty: true,
+    kalawaoCounty: false,
+    kauaiCounty: false,
+    mauiCounty: false,
+    hawaiiCounty: false,
+  });
 
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
-    }
-  };
+  onHawaiiClick = (props, marker) => this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true,
+    hawaiiCounty: true,
+    kalawaoCounty: false,
+    kauaiCounty: false,
+    honoluluCounty: false,
+    mauiCounty: false,
+  });
+
+  onMauiClick = (props, marker) => this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true,
+    mauiCounty: true,
+    kalawaoCounty: false,
+    kauaiCounty: false,
+    honoluluCounty: false,
+    hawaiiCounty: false,
+  });
+
+  onKauaiClick = (props, marker) => this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true,
+    kauaiCounty: true,
+    kalawaoCounty: false,
+    honoluluCounty: false,
+    mauiCounty: false,
+    hawaiiCounty: false,
+  });
+
+  onKalawaoClick = (props, marker) => this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true,
+    kalawaoCounty: true,
+    kauaiCounty: false,
+    honoluluCounty: false,
+    mauiCounty: false,
+    hawaiiCounty: false,
+  });
+
+  // showHonolulu = () => this.setState({
+  //   showingInfoWindow: true,
+  // });
+  //
+  // closeHonolulu = () => this.setState({
+  // showingInfoWindow: false,
+  //   activeMarker: null,
+  // });
+
+  // onClose = props => {
+  //   if (this.state.showingInfoWindow) {
+  //     this.setState({
+  //       showingInfoWindow: false,
+  //       activeMarker: null,
+  //     });
+  //   }
+  // };
 
   render() {
 
@@ -50,31 +109,33 @@ export class MapContainer extends React.Component {
               lng: -157.5,
             }}
         >
-          <Marker onHover={this.onMarkerClick}
-                  name={'Current location'}
+          <Marker onClick={this.onHonoluluClick}
+                  // onMouseEnter={() => this.state.showingInfoWindow(true)}
+                  // onMouseExit={() => this.state.showingInfoWindow(false)}
+                  name={'Honolulu County'}
                   position={{ lat: 21.4389, lng: -158.0001 }}
           />
-          {/*<InfoWindow*/}
-          {/*    marker={this.state.activeMarker}*/}
-          {/*    visible={this.state.showingInfoWindow}>*/}
-          {/*  <div>*/}
-          {/*    <h1>Honolulu County</h1>*/}
-          {/*  </div>*/}
-          {/*</InfoWindow>*/}
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'}
+           <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>Honolulu County</h1>
+            </div>
+           </InfoWindow>
+          <Marker onClick={this.onHawaiiClick}
+                  name={'Hawaii County'}
                   position={{ lat: 19.5429, lng: -155.6659 }}
           />
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'}
+          <Marker onClick={this.onMauiClick}
+                  name={'Maui County'}
                   position={{ lat: 20.7984, lng: -156.3319 }}
           />
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'}
+          <Marker onClick={this.onKauaiClick}
+                  name={'Kauai County'}
                   position={{ lat: 21.9661, lng: -159.5738 }}
           />
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'}
+          <Marker onClick={this.onKalawaoClick}
+                  name={'Kalawao County'}
                   position={{ lat: 21.1950, lng: -156.9750 }}
           />
         </Map>
@@ -83,5 +144,5 @@ export class MapContainer extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'INSERT API KEY HERE',
+  apiKey: 'AIzaSyAbV-z-8r_Ez5Pv1sfjoMdGy4V600Y1yKw',
 })(MapContainer);
