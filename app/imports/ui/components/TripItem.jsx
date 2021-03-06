1635b7fa9891f906bc4b6bca39c2d52bf448ef87
Louella Caraang <lcaraang@hawaii.edu>
2021-03-06 12:18:38 -1000

    Add county to trip schema

--- app/imports/ui/components/TripItem.jsx
+++ app/imports/ui/components/TripItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Trip table. See pages/ListTrip.jsx. */
 class TripItem extends React.Component {
@@ -15,6 +14,8 @@ class TripItem extends React.Component {
           <Table.Cell>{this.props.trip.distance}</Table.Cell>
           <Table.Cell>{(this.props.trip.distance / this.props.trip.mpg).toFixed(2)}</Table.Cell>
           <Table.Cell>{((this.props.trip.distance / this.props.trip.mpg) * 19.6).toFixed(2)}</Table.Cell>
+          <Table.Cell>{this.props.trip.owner}</Table.Cell>
+          <Table.Cell>{this.props.trip.county}</Table.Cell>
         </Table.Row>
     );
   }
@@ -25,5 +26,4 @@ TripItem.propTypes = {
   trip: PropTypes.object.isRequired,
 };
 
-/** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(TripItem);
+export default TripItem;
--- app/imports/ui/pages/Community.jsx
+++ app/imports/ui/pages/Community.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Grid, Statistic, Icon, Progress, Divider, Header, Card } from 'semantic-ui-react';
+import { Divider, Header, Card } from 'semantic-ui-react';
 import SidebarVisible from '../components/SideBar';
 import Map from '../components/Map';
+import County from '../components/County';
 
 class Community extends React.Component {
   render() {
@@ -12,49 +13,7 @@ class Community extends React.Component {
         <Map/>
        </div>
        <div id='community-bottom'>
-       <Grid centered>
-         <Grid.Row>
-           <Grid.Column as="h2" textAlign='center'>Honolulu County</Grid.Column>
-         </Grid.Row>
-         <Grid.Row>
-           <Grid.Column width={5}>    <Statistic>
-             <Statistic.Value>
-               <Icon name='users' />50
-             </Statistic.Value>
-             <Statistic.Label>users</Statistic.Label>
-           </Statistic>
-           </Grid.Column>
-             <Grid.Column width={5}>
-             <Progress value='50' total='100' progress='percent' label="2021 GOAL: 100 USERS" color="blue"/></Grid.Column>
-         </Grid.Row>
-         <Grid.Row>
-           <Grid.Column width={5}>    <Statistic>
-             <Statistic.Value><Icon name='fire'/>200</Statistic.Value>
-             <Statistic.Label>gallons of gas saved</Statistic.Label>
-           </Statistic>
-           </Grid.Column>
-           <Grid.Column width={5}>
-             <Progress value='200' total='1000' progress='percent' label="2021 GOAL: 1,000 GALLONS OF GAS SAVED" color="blue"/></Grid.Column>
-         </Grid.Row>
-         <Grid.Row>
-           <Grid.Column width={5}>    <Statistic>
-             <Statistic.Value><Icon name='cloud'/>3,000</Statistic.Value>
-             <Statistic.Label>pounds of CO2 reduced</Statistic.Label>
-           </Statistic>
-           </Grid.Column>
-           <Grid.Column width={5}>
-             <Progress value='3000' total='10000' progress='percent' label="2021 GOAL: 10,000 POUNDS OF CO2 REDUCED" color="blue"/></Grid.Column>
-         </Grid.Row>
-         <Grid.Row>
-           <Grid.Column width={5}>    <Statistic>
-             <Statistic.Value><Icon name='car'/>5,000</Statistic.Value>
-             <Statistic.Label>vehicle miles traveled (VMT) reduced</Statistic.Label>
-           </Statistic>
-           </Grid.Column>
-           <Grid.Column width={5}>
-             <Progress value='5000' total='20000' progress='percent' label="2021 GOAL: 20,000 VMT REDUCED" color="blue"/></Grid.Column>
-         </Grid.Row>
-       </Grid>
+       <County/>
        <Divider horizontal>
          <Header as='h3'>
            Get Involved
