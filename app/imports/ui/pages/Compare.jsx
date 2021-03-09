7667c7adfdb9a3797b956f3e3e0baaccefbdb476
Tiffany Williams <60216343+tiffanywilliams@users.noreply.github.com>
2021-03-09 00:46:25 -1000

    changes to the compare page

--- app/imports/ui/pages/Compare.jsx
+++ app/imports/ui/pages/Compare.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import { Grid, Header, Image, Card } from 'semantic-ui-react';
+import { Grid, Header, Image, Card, Divider } from 'semantic-ui-react';
 import SidebarVisible from '../components/SideBar';
 
 function SliderHandler() {
@@ -26,20 +26,44 @@ function SliderHandler() {
     });
   }, [totalMiles]);
 
+  const testMPG = 25;
+  const avgGasPrice = 3.52;
+  const gasCost = (100 / testMPG) * avgGasPrice;
+  const costPerkWh = 0.2874;
+
   return (
       <Slider className='compare-slider'>
         {evData.map((value, index) => <Slide index={index} key={index}>
-          <Grid columns={2} divided>
+          <Grid centered columns={2} divided>
             <Grid.Column width={6}>
               <Image className='compare-image-card' src={value.brandLogo} size='small'/>
               <Image className='compare-image-card' src={value.image} size='large' href={value.website}
                      target='_blank'/>
               <Header textAlign='center'>{value.car}</Header>
             </Grid.Column>
-            <Grid.Column className='slider-information-side'>
-              <Grid.Row>Starting From: ${value.cashPrice}</Grid.Row>
-              <Grid.Row>Range: {value.range} mi</Grid.Row>
-              <Grid.Row>Battery Capacity: {value.batteryCapacity} kWh</Grid.Row>
+            <Grid.Column width={10} className='slider-information-side'>
+              <Grid.Row textAlign='left'>Starting From: ${value.cashPrice}</Grid.Row>
+              <Grid.Row textAlign='left'>Range: {value.range} mi</Grid.Row>
+              <Grid.Row textAlign='left'>Battery Capacity: {value.batteryCapacity} kWh</Grid.Row>
+              <Grid.Row>
+                  <Header className='cost-per-header' textAlign='center'>Cost Per 100 mi</Header>
+                  <Card.Group className='cost-per-inner-cards' centered itemsPerRow={2}>
+                    <Card fluid>
+                      <Card.Content textAlign='center'>
+                        Electric Vehicle
+                        <Divider/>
+                        ${((100 / (value.range / value.batteryCapacity)) * costPerkWh).toFixed(2)}
+                      </Card.Content>
+                    </Card>
+                    <Card fluid>
+                      <Card.Content textAlign='center'>
+                        Gas
+                        <Divider />
+                        ${gasCost.toFixed(2)}
+                      </Card.Content>
+                    </Card>
+                  </Card.Group>
+              </Grid.Row>
             </Grid.Column>
           </Grid>
         </Slide>)}
