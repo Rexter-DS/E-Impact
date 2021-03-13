bff6414df4970efc81c5687d46c28d100b12edeb
Kameron Wong <wongk711@hawaii.edu>
2021-03-12 16:40:47 -1000

    Update Daily Page

--- app/imports/ui/components/TripItem.jsx
+++ app/imports/ui/components/TripItem.jsx
@@ -1,28 +1,66 @@
-import React from 'react';
-import { Table } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Table, Button, Confirm } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { Trips } from '../../api/trip/TripCollection';
+import { Users } from '../../api/user/UserCollection';
 
 /** Renders a single row in the List Trip table. See pages/ListTrip.jsx. */
-class TripItem extends React.Component {
-  render() {
-    let gallons;
-    if (this.props.trip.mode === 'Gas Car' || this.props.trip.mode === 'Carpool') {
-      gallons = (this.props.trip.distance !== 0 ? ((this.props.trip.distance / this.props.trip.mpg)) : 0);
-    } else {
-      gallons = 0;
-    }
-    const ghg = gallons === 0 ? 0 : gallons * 19.6;
-    return (
-        <Table.Row>
-          <Table.Cell>{this.props.trip.date.toLocaleDateString()}</Table.Cell>
-          <Table.Cell>{this.props.trip.mode}</Table.Cell>
-          <Table.Cell>{this.props.trip.distance}</Table.Cell>
-          <Table.Cell>{this.props.trip.mpg}</Table.Cell>
-          <Table.Cell>{gallons === 0 ? 0 : `${gallons.toFixed(2)} gal`}</Table.Cell>
-          <Table.Cell>{ghg === 0 ? 0 : `${ghg.toFixed(2)} lbs`}</Table.Cell>
-        </Table.Row>
-    );
+const TripItem = (props) => {
+  let gallons;
+  const tripMpg = props.trip.mpg > 0 ? props.trip.mpg : Users.getUserProfile(Meteor.user().username)?.autoMPG.isDefined() ? Users.getUserProfile(Meteor.user().username).autoMPG : 25;
+  if (props.trip.mode === 'Gas Car' || props.trip.mode === 'Carpool') {
+    gallons = (props.trip.distance !== 0 ? ((props.trip.distance / tripMpg)) : 0);
+  } else {
+    gallons = -(props.trip.distance !== 0 ? ((props.trip.distance / tripMpg)) : 0);
   }
+  const ghg = gallons === 0 ? 0 : gallons * 19.6;
+
+  const galStyle = gallons > 0 ? { color: 'red' } : { color: 'green' };
+  const ghgStyle = ghg > 0 ? { color: 'red' } : { color: 'green' };
+
+  const [confirmState, setConfirmState] = useState(false);
+
+  function handleClickDel() {
+    Trips.removeIt(props.trip);
+  }
+
+  function handleCancel() {
+    setConfirmState(false);
+  }
+
+  function handleConfirm() {
+    handleClickDel();
+    setConfirmState(false);
+  }
+
+  function openConfirm() {
+    setConfirmState(true);
+  }
+
+  function abs(val) {
+    if (val < 0) {
+      return -1 * val;
+    }
+    return val;
+  }
+
+  return (
+      <Table.Row>
+        <Table.Cell>{props.trip.date.toLocaleDateString()}</Table.Cell>
+        <Table.Cell>{props.trip.mode}</Table.Cell>
+        <Table.Cell>{props.trip.distance}</Table.Cell>
+        <Table.Cell>{props.trip.mpg}</Table.Cell>
+        <Table.Cell style={galStyle}>{gallons === 0 ? 0 : `${abs(gallons).toFixed(2)} gal`}</Table.Cell>
+        <Table.Cell style={ghgStyle}>{ghg === 0 ? 0 : `${abs(ghg).toFixed(2)} lbs`}</Table.Cell>
+        <Table.Cell><Button negative circular icon='x' onClick={openConfirm}></Button><Confirm
+            open={confirmState}
+            header='Delete Trip?'
+            onCancel={handleCancel}
+            onConfirm={handleConfirm}
+        /></Table.Cell>
+      </Table.Row>
+  );
 }
 
 /** Require a document to be passed to this component. */
--- app/imports/ui/pages/Daily.jsx
+++ app/imports/ui/pages/Daily.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import { Menu, Button, Table, Grid, Loader } from 'semantic-ui-react';
+import { Menu, Button, Table, Grid, Loader, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import SideBar from '../components/SideBar';
@@ -9,59 +9,121 @@ import { Trips, tripPublications } from '../../api/trip/TripCollection';
 import TripItem from '../components/TripItem';
 import Footer from '../components/Footer';
 
-class Daily extends React.Component {
-
-  render() {
-    return this.props.ready ? this.renderPage() : <Loader active>Loading data</Loader>;
-  }
-
-  renderPage() {
-    // const miles = this.props.trips[0].distance;
-    const month = 3; // march
-    const monthTrips = _.filter(this.props.trips, (trip) => trip.date.getMonth() + 1 === month).sort((a, b) => ((a.date < b.date) ? 1 : -1))
-    let monthlySum = 0;
-    for (let i = 0; i < monthTrips.length; i++) {
-      const add = (monthTrips[i].distance / monthTrips[i].mpg) * 19.6;
-      if (!isNaN(add) && isFinite(add)) {
+const Daily = (props) => {
+  const monthString = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const today = new Date();
+  const [currentMonthYr, setCurrentMonthYr] = useState([today.getMonth(), today.getFullYear()]);
+  const monthTrips = _.filter(props.trips, (trip) => trip.date.getMonth() === currentMonthYr[0] && trip.date.getFullYear() === currentMonthYr[1]).sort((a, b) => ((a.date < b.date) ? 1 : -1));
+  let monthlySum = 0;
+  for (let i = 0; i < monthTrips.length; i++) {
+    const tripMpg = monthTrips[i].mpg > 0 ? monthTrips[i].mpg : 25;
+    const add = (monthTrips[i].distance / tripMpg) * 19.6;
+    if (!isNaN(add) && isFinite(add)) {
+      if (monthTrips[i].mode === 'Gas Car' || monthTrips[i].mode === 'Carpool') {
         monthlySum += add;
+      } else {
+        monthlySum -= add;
       }
     }
-
-    return (
-        <div id='daily-container' style={{ marginLeft: '150px' }}>
-          <SideBar/>
-          <Menu borderless id="daily-top">
-            <Grid style={{ width: '100%' }}>
-              <Grid.Column width={4} verticalAlign='bottom'><Menu.Item className='daily-header-sums'>February
-                2021</Menu.Item></Grid.Column>
-              <Grid.Column width={6} textAlign='center'><Menu.Item className='daily-header-sums'>Total GHG Produced: {monthlySum.toFixed(2)}</Menu.Item></Grid.Column>
-
-              <Grid.Column width={4} textAlign='center'><Menu.Item className='daily-header-sums'></Menu.Item></Grid.Column>
-              <Grid.Column width={2} verticalAlign='bottom'><Menu.Item position='right'>
-                <Button className='daily-add-button' href={'#/addTrip'}>Add</Button>
-              </Menu.Item></Grid.Column>
-            </Grid>
-          </Menu>
-          <Table fixed className='daily-table'>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell className='daily-table-header'>Date</Table.HeaderCell>
-                <Table.HeaderCell className='daily-table-header'>Mode of Transportation</Table.HeaderCell>
-                <Table.HeaderCell className='daily-table-header'>Distance</Table.HeaderCell>
-                <Table.HeaderCell className='daily-table-header'>mpg</Table.HeaderCell>
-                <Table.HeaderCell className='daily-table-header'>Gallons used</Table.HeaderCell>
-                <Table.HeaderCell className='daily-table-header'>GHG produced</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {monthTrips.map((trip) => <TripItem key={trip._id} trip={trip} />)}
-            </Table.Body>
-          </Table>
-          <Footer id={'daily-footer'}/>
-        </div>
-    );
   }
-}
+  const monthlySumStyle = monthlySum > 0 ? { color: 'red' } : { color: 'green' };
+
+  function handleClickNext() {
+    const m = (currentMonthYr[0] + 1) % 12;
+    let y = currentMonthYr[1];
+    if (m === 0) {
+      y = currentMonthYr[1] + 1;
+    }
+    setCurrentMonthYr([m, y])
+  }
+
+  function handleClickPrev() {
+    const m = (currentMonthYr[0] + 11) % 12;
+    let y = currentMonthYr[1];
+    if (m === 11) {
+      y -= 1;
+    }
+    setCurrentMonthYr([m, y])
+  }
+
+  function handleClickToday() {
+    setCurrentMonthYr([today.getMonth(), today.getFullYear()]);
+  }
+
+  function abs(val) {
+    if (val < 0) {
+      return -1 * val;
+    }
+    return val;
+  }
+
+  return !props.ready ? <Loader active>Loading data</Loader> :
+        (
+            <div id='daily-container'
+                 style={{ marginLeft: '150px' }}>
+              <SideBar/>
+              <Menu borderless
+                    id="daily-top">
+                <Menu.Item>
+                <Button className='daily-today-button' style={{ fontSize: '16px' }} onClick={handleClickToday}>
+                  This Month
+                </Button>
+                </Menu.Item>
+                <Grid style={{ width: '100%' }}>
+                  <Grid.Column width={4} verticalAlign='middle'>
+                    <Menu.Item className='daily-header-sums'>
+                      <Button className='daily-arrow-button' circular animated={'fade'} onClick={handleClickPrev}>
+                        <Button.Content visible><Icon className='arrow left icon'/></Button.Content>
+                        <Button.Content hidden>
+                          Prev
+                        </Button.Content>
+                      </Button>
+                        {`${monthString[currentMonthYr[0]]} ${currentMonthYr[1]}`}
+                      <Button className='daily-arrow-button' circular animated={'fade'} onClick={handleClickNext}>
+                        <Button.Content visible><Icon className='arrow right icon'/></Button.Content>
+                        <Button.Content hidden>
+                          Next
+                        </Button.Content>
+                      </Button>
+                  </Menu.Item>
+                  </Grid.Column>
+                  <Grid.Column width={6} textAlign='center'><Menu.Item className='daily-header-sums'>Total Net GHG:</Menu.Item>
+                    <Menu.Item className='daily-header-sums' style={monthlySumStyle}>
+                      {abs(monthlySum).toFixed(2)} lbs {monthlySum === 0 ? '' : monthlySum > 0 ? 'Produced' : 'Reduced'}
+                    </Menu.Item></Grid.Column>
+
+                  <Grid.Column width={4}
+                               textAlign='center'><Menu.Item className='daily-header-sums'></Menu.Item></Grid.Column>
+                  <Grid.Column width={2}
+                               verticalAlign='middle'><Menu.Item>
+                    <Button className='daily-add-button'
+                            href={'#/addTrip'}>Add</Button>
+                  </Menu.Item></Grid.Column>
+                </Grid>
+              </Menu>
+              <Table fixed
+                     className='daily-table'>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className='daily-table-header'>Date</Table.HeaderCell>
+                    <Table.HeaderCell className='daily-table-header'>Mode of Transportation</Table.HeaderCell>
+                    <Table.HeaderCell className='daily-table-header'>Distance</Table.HeaderCell>
+                    <Table.HeaderCell className='daily-table-header'>mpg</Table.HeaderCell>
+                    <Table.HeaderCell className='daily-table-header'>Net Gallons</Table.HeaderCell>
+                    <Table.HeaderCell className='daily-table-header'>Net GHG</Table.HeaderCell>
+                    <Table.HeaderCell className='daily-table-header'>Delete Trip</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {monthTrips.map((trip) => <TripItem key={trip._id} trip={trip}/>)}
+                  <Table.Row>
+                    <Table.Cell>{`${monthTrips.length > 0 ? monthTrips.length : 'No'} Trips listed`}</Table.Cell></Table.Row>
+                </Table.Body>
+              </Table>
+              <Footer id={'daily-footer'}/>
+            </div>
+        );
+  }
 
 Daily.propTypes = {
   ready: PropTypes.bool.isRequired,
