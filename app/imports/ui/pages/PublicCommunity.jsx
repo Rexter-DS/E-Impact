940d5165b21bac1905f917f9ee9fef106811a502
Jake Camarao <jcamarao@hawaii.edu>
2021-03-17 14:32:33 -1000

    organization descriptions done

--- app/imports/ui/pages/PublicCommunity.jsx
+++ app/imports/ui/pages/PublicCommunity.jsx
@@ -15,94 +15,139 @@ class PublicCommunity extends React.Component {
               <PublicMap/>
             </div>
             <div id='public-community-bottom'>
-              <h1 id='community-engagement' align='center'>Get involved with the community</h1>
+              <div id='community-engagement'>
+              <h1 id='community-engagement-header' align='center'>Get involved with the community</h1>
+              <p id='community-engagement-paragraph'>
+                We believe that Hawaii is home for everybody! This includes the flora and fauna.
+                Work with neighbors, coworkers, family, and help support the community to make Hawaii a better place.
+                There are a lot of welcoming organizations around the state helping the initiative to keep Hawaii green.
+                We invite you to interact with the map, learn more about the organizations near your area, and contribute to a better Hawaii.
+                Let&apos;s all find a way to make an environmental impact in a positive way!
+              </p>
+              </div>
               <Grid>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/sustainable-coastlines.png"/>
-                  <a href='https://www.sustainablecoastlineshawaii.org/' target='_blank'>Sustainable Coastlines Hawaii</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://www.sustainablecoastlineshawaii.org/' target='_blank'>Sustainable Coastlines Hawaii</a>
+                  </div>
                   <p>
-                  Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                  Conserving Hawaii&apos;s lands and waters by hosting beach cleanups.
+                    Educating our communities to sustain better green lifestyles.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/hawaii-conservation-alliance.png"/>
-                  <a href='https://www.hawaiiconservation.org/' target='_blank'>Hawaii Conservation Alliance</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://www.hawaiiconservation.org/' target='_blank'>Hawaii Conservation Alliance</a>
+                  </div>
                   <p>
-                  Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                  Combines science and hardwork to perserve Hawaii&apos;s ecosystems.
+                    Continuous research lifts our knowledge to allow the state to thrive.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/808cleanups.png"/>
-                  <a href='https://808cleanups.org/' target='_blank'>808 Cleanups</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://808cleanups.org/' target='_blank'>808 Cleanups</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Engaging the community to work together to clean up the islands.
+                    Building a greener future for Hawaii&apos;s youth.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/hawaii-wildlife-fund.png"/>
-                  <a href='https://www.wildhawaii.org/' target='_blank'>Hawaii Wildlife Fund</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://www.wildhawaii.org/' target='_blank'>Hawaii Wildlife Fund</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Saving hawaii&apos;s coastal wildlife by advocacy, research, and community engagement.
+                    Teaching Hawaii&apos;s next generation green practices.
                   </p>
                 </Grid.Column>
               </Grid>
               <Grid>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/sierra-club.png"/>
-                  <a href='https://sierraclubhig.org/' target='_blank'>Sierra Club Hawaii</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://sierraclubhig.org/' target='_blank'>Sierra Club Hawaii</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Hawaii&apos;s local branch of a nation wide initiative to preserve our planet.
+                    One of the oldest organizations keeping Hawaii green.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/marine-resource-council.png"/>
-                  <a href='https://www.mauireefs.org/' target='_blank'>Marine Resource Council</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://www.mauireefs.org/' target='_blank'>Marine Resource Council</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Preserving our waters and reefs to be crystal clear and full of live.
+                    Keeping maui clean and the community safe by research.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/waiwai-ola.png"/>
-                  <a href='https://waterkeepershi.org/' target='_blank'>Waiwai Ola Waterkeepers Hawaii</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://waterkeepershi.org/' target='_blank'>Waiwai Ola Waterkeepers Hawaii</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Cleansing the water quality statewide through determination and beautification.
+                    Saving native oysters and marine wildlife.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/kokee-resource.png"/>
-                  <a href='https://www.krcp.org/' target='_blank'>Kōkeʻe Resource Conservation Program</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://www.krcp.org/' target='_blank'>Kōkeʻe Resource Conservation Program</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Saving Kauai&apos;s native plants by safely removing invasive plant species.
+                    Promoting the forest&apos;s biodiversity in an engaging manner.
                   </p>
                 </Grid.Column>
               </Grid>
               <Grid>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/kisc.png"/>
-                  <a href='https://www.kauaiisc.org/' target='_blank'>Kauaʻi Invasive Species Committee</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://www.kauaiisc.org/' target='_blank'>Kauaʻi Invasive Species Committee</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Saving Kauai&apos;s plant and animal species from facing extinction.
+                    Working with lawmakers to remove invasive species harming the biodiversity.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/ka-honua-momona.png"/>
-                  <a href='https://kahonuamomona.org/' target='_blank'>Ka Honua Momona</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://kahonuamomona.org/' target='_blank'>Ka Honua Momona</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Based in Molokai, promoting sustainability through old practices.
+                    Educating and fostering community engagement to preserve Hawaii&apos;s cultural heritage.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/lanai-culture.png"/>
-                  <a href='https://lanaichc.org/' target='_blank'>Lāna‘i Culture & Heritage Center</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://lanaichc.org/' target='_blank'>Lāna‘i Culture & Heritage Center</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Promoting community engagement by spreading cultural awareness.
+                    Teaching safe land practices and cultural literacy for the Lanai&apos;s community.
                   </p>
                 </Grid.Column>
-                <Grid.Column className='organizations' width={4}>
+                <Grid.Column width={4}>
                   <Image size='medium' src="/organizations/ke-ao-halii.png"/>
-                  <a href='https://savehanacoast.org/' target='_blank'>Ke Ao Hali‘i</a>
+                  <div className='organizations'>
+                    <a rel='noreferrer' className='organization-name' href='https://savehanacoast.org/' target='_blank'>Ke Ao Hali‘i</a>
+                  </div>
                   <p>
-                    Conserving Hawaii&apos;s lands and waters. Hosts community cleanups and assists others host their own cleanups.
+                    Maintaining Hāna Hawaii land to conserving cultural heritage and the environment.
+                    Educating the community by teaching responsible coastal usage and practices.
                   </p>
                 </Grid.Column>
               </Grid>
