7d3cebadbcbe6f21313e793a404be96c38d28528
jklaulau <jklau88@hawaii.edu>
2021-03-19 18:18:43 -1000

    Background image now covers white space

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -10,7 +10,6 @@ class Landing extends React.Component {
     return (
         <div id="landing">
           <LandingNavBar/>
-
           {/* This controls the top half of the landing page */}
           <div style={{ paddingLeft: '250px', paddingRight: '250px' }} className="ui center aligned container">
             <p>Help keep our air clean and monitor your Green House Gas emissions!</p>
@@ -39,8 +38,9 @@ class Landing extends React.Component {
               </Button>
             </Grid.Column>
           </Grid>
-        <Footer id={'landing-footer'}/>
+            <Footer id={'landing-footer'}/>
         </div>
+
     );
   }
 }
