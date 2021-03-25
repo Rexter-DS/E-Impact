6aee31fcee4bcc0cbbb1373a9e0131a34ac12ae2
jklaulau <jklau88@hawaii.edu>
2021-03-24 14:59:42 -1000

    Updated landing

--- app/imports/ui/components/Info1.jsx
+++ app/imports/ui/components/Info1.jsx
@@ -7,10 +7,19 @@ class Info1 extends React.Component {
     render() {
         return (
             <div id='landing-info1'>
-                <h1>Use our GHC estimator to calculate your GHG emissions for a single trip</h1>
-                <Button as={NavLink} exact to='/quickaccess'>
-                    <Button.Content visible>Show me!</Button.Content>
-                </Button>
+                <Grid divided stackable inverted>
+                    <Grid.Row columns={2} style={{ padding:'25px' }}>
+                        <Grid.Column verticalAlign='middle'>
+                            <h1>Use our GHC estimator to calculate your GHG emissions for a single trip</h1>
+                            <Button as={NavLink} exact to='/quickaccess'>
+                                <Button.Content visible>Show me!</Button.Content>
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column className='landing-info-center' verticalAlign='middle'>
+                            <Icon disabled name='calculator' size='massive'/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         );
     }
--- app/imports/ui/components/Info2.jsx
+++ app/imports/ui/components/Info2.jsx
@@ -7,13 +7,20 @@ class Info2 extends React.Component {
     render() {
         return (
             <div id='landing-info1'>
-                <h1>Create an account to keep track of the emissions of your daily transit</h1>
-                <Button as={NavLink} exact to='/signup' animated>
-                    <Button.Content visible>Take me there!</Button.Content>
-                    <Button.Content hidden>
-                        <Icon name='long arrow alternate right'/>
-                    </Button.Content>
-                </Button>
+                <Grid>
+                    <Grid.Column width={4}>
+                        <Icon disabled name='chain' size='massive'/>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <h1>Create an account to keep track of the emissions of your daily transit</h1>
+                        <Button as={NavLink} exact to='/signup' animated>
+                            <Button.Content visible>Take me there!</Button.Content>
+                            <Button.Content hidden>
+                                <Icon name='long arrow alternate right'/>
+                            </Button.Content>
+                        </Button>
+                    </Grid.Column>
+                </Grid>
             </div>
         );
     }
