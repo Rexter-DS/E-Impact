e715831364d2789ceb3020136178e7dedaa85c3d
justin-mar <70727597+justin-mar@users.noreply.github.com>
2021-03-30 22:12:21 -1000

    changed date select to event select

--- app/imports/ui/components/ChoseScenario.jsx
+++ app/imports/ui/components/ChoseScenario.jsx
@@ -19,9 +19,7 @@ function ChoseScenario(
     },
 ) {
 
-    /*
-        Creates array from milesSavedPerDay's fields for use in fullcalendar
-     */
+    // Creates array from milesSavedPerDay's fields for use in fullcalendar
     const events = _.map(milesSavedPerDay.date, function (date, i) {
         const year = `${date.getFullYear()}`;
         let month = `${date.getMonth() + 1}`;
@@ -38,33 +36,82 @@ function ChoseScenario(
         return { id: i, title: milesSavedPerDay.mode[i], date: [year, month, day].join('-') };
     });
 
-    // save date user selects
-    const handleCalendarSelect = (info) => {
-        swal(`selected ${info.startStr} to ${info.endStr}`);
+    const handleEventSelect = (info) => {
+        swal(`Events: ${info.event.id} ${info.event.title} ${info.event.start}`);
+        const eventDate = info.event.start.toString();
+        const year = eventDate.slice(11, 15);
+        let month = eventDate.slice(4, 7);
+        switch (month) {
+            case 'Jan':
+                month = '01';
+                break;
+            case 'Feb':
+                month = '02';
+                break;
+            case 'Mar':
+                month = '03';
+                break;
+            case 'Apr':
+                month = '04';
+                break;
+            case 'May':
+                month = '05';
+                break;
+            case 'Jun':
+                month = '06';
+                break;
+            case 'Jul':
+                month = '07';
+                break;
+            case 'Aug':
+                month = '08';
+                break;
+            case 'Sep':
+                month = '09';
+                break;
+            case 'Oct':
+                month = '10';
+                break;
+            case 'Nov':
+                month = '11';
+                break;
+            case 'Dec':
+                month = '12';
+                break;
+            default:
+                break;
+        }
+        const day = eventDate.slice(8, 10);
+        const selectedEvent = { id: info.event.id, title: info.event.title, date: [year, month, day].join('-') };
     };
 
+    // state for transport user selected in form
     const [transport, setTransport] = useState();
 
+    // on clicking submit button, swal message appears
     const handleSubmit = (evt) => {
         evt.preventDefault();
         swal('Success');
     };
 
+    // updates selected state transport
     const handleChange = (event, { value }) => setTransport(value);
 
     return (
         <div>
+            {/* renders fullcalendar */}
             <div id='calendar-container'>
                 <FullCalendar
                     plugins={[dayGridPlugin, interactionPlugin]}
                     initialView='dayGridMonth'
                     contentHeight={400}
                     selectable={true}
-                    select={handleCalendarSelect}
+                    eventClick={handleEventSelect}
                     events={events}
                 />
             </div>
 
+            {/* render form to select mode of transportation */}
             <Form id='calendar-form' onSubmit={handleSubmit}>
                 <Form.Radio
                     label='Bike'
