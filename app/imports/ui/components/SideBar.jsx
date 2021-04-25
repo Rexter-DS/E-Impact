edad451c0048ce8e431d28a854478e3b3b1ecf4b
Rexter Delos Santos <rexterds@hawaii.edu>
2021-04-24 22:28:24 -1000

    Update SideBar.jsx

--- app/imports/ui/components/SideBar.jsx
+++ app/imports/ui/components/SideBar.jsx
@@ -21,8 +21,6 @@ const handleChange = () => {
 
 const SideBar = (props) => {
 
-  const [settings, setSettings] = useState(false);
-
   if (props.userReady && (document.getElementById('sidebar'))) {
     // console.log(document.getElementsByClassName('sidebar-item'));
 
@@ -45,128 +43,130 @@ const SideBar = (props) => {
         sidebarItems[i].classList.remove('dark-sidebar-item');
       }
     }
-
-    useEffect(() => {
-      const dashboardModals = document.getElementsByClassName('card-modal');
-      if (props.userProfile.theme === 'dark') {
-        for (let i = 0; i < dashboardModals.length; i++) {
-          dashboardModals[i].classList.add('dark-card');
-        }
-      } else {
-        for (let i = 0; i < dashboardModals.length; i++) {
-          dashboardModals[i].classList.remove('dark-card');
-        }
-      }
-    }, [props.userProfile, settings]);
   }
+
+  const [settings, setSettings] = useState(false);
+
+  useEffect(() => {
+    const dashboardModals = document.getElementsByClassName('card-modal');
+    if (props.userProfile.theme === 'dark') {
+      for (let i = 0; i < dashboardModals.length; i++) {
+        dashboardModals[i].classList.add('dark-card');
+      }
+    } else {
+      for (let i = 0; i < dashboardModals.length; i++) {
+        dashboardModals[i].classList.remove('dark-card');
+      }
+    }
+  }, [props.userProfile, settings]);
 
   return (
       props.currentUser ? <div><Sidebar
-          as={Menu}
-          animation='overlay'
-          icon='labeled'
-          vertical
-          visible
-          width='thin'
-          id='sidebar'
-      >
-        <Menu.Item
-            style={{ padding: 0 }}>
-          <Image size='large'
-                 src={this.sidebarLogo}
-          />
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName=""
-                   exact
-                   to={`/Dashboard/${Meteor.user()?.username}`}>
-          <Icon name='grid layout'/>
-          Dashboard
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName="active"
-                   exact
-                   to="/daily"
-                   key='daily'>
-          <Icon name='list'/>
-          Daily
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName=""
-                   exact
-                   to={`/WhatIf/${Meteor.user()?.username}`}>
-          <Icon name='grid layout'/>
-          What If
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName="active"
-                   exact
-                   to="/compare"
-                   key='compare'>
-          <Icon name='car'/>
-          Compare
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName="active"
-                   exact to="/community"
-                   key='community'>
-          <Icon name='globe'/>
-          Community
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName="active"
-                   exact
-                   to="/admin"
-                   key='admin'>
-          <Icon name='cog'/>
-          Admin Page
-        </Menu.Item>
-        <Menu.Item as={NavLink}
-                   className='sidebar-item'
-                   activeClassName="active"
-                   exact
-                   to="/"
-                   key='signout'>
-          <Icon name='sign-out'/>
-          Sign Out
-        </Menu.Item>
-        <Menu.Item>
-          <DarkModeToggle
-              checked={props.theme === 'dark'}
-              onChange={handleChange}
-              size={60}/>
-        </Menu.Item>
-        <Modal
-            closeIcon
-            open={settings}
-            onOpen={() => setSettings(true)}
-            onClose={() => setSettings(false)}
-            trigger={
-              <Menu.Item
-                  className='sidebar-item'
-                  style={{ bottom: '0' }}>
-                <Icon name='user circle outline'/>
-                {Meteor.user() ? Meteor.user().username : 'Guest'}
-              </Menu.Item>
-            }
-        >
-          <Header className='card-modal'>Settings</Header>
-          <Modal.Content className='card-modal'>
-            <Settings
-                docId={props.userProfile._id}
-                username={props.userProfile.username}
-                userMpg={props.userProfile.autoMPG}
-                userHomeRoundTrip={props.userProfile.homeRoundTrip}
-            />
-          </Modal.Content>
-        </Modal>
-      </Sidebar></div>
+              as={Menu}
+              animation='overlay'
+              icon='labeled'
+              vertical
+              visible
+              width='thin'
+              id='sidebar'
+          >
+            <Menu.Item
+                style={{ padding: 0 }}>
+              <Image size='large'
+                     src={this.sidebarLogo}
+              />
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName=""
+                       exact
+                       to={`/Dashboard/${Meteor.user()?.username}`}>
+              <Icon name='grid layout'/>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName="active"
+                       exact
+                       to="/daily"
+                       key='daily'>
+              <Icon name='list'/>
+              Daily
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName=""
+                       exact
+                       to={`/WhatIf/${Meteor.user()?.username}`}>
+              <Icon name='grid layout'/>
+              What If
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName="active"
+                       exact
+                       to="/compare"
+                       key='compare'>
+              <Icon name='car'/>
+              Compare
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName="active"
+                       exact to="/community"
+                       key='community'>
+              <Icon name='globe'/>
+              Community
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName="active"
+                       exact
+                       to="/admin"
+                       key='admin'>
+              <Icon name='cog'/>
+              Admin Page
+            </Menu.Item>
+            <Menu.Item as={NavLink}
+                       className='sidebar-item'
+                       activeClassName="active"
+                       exact
+                       to="/"
+                       key='signout'>
+              <Icon name='sign-out'/>
+              Sign Out
+            </Menu.Item>
+            <Menu.Item>
+              <DarkModeToggle
+                  checked={props.theme === 'dark'}
+                  onChange={handleChange}
+                  size={60}/>
+            </Menu.Item>
+            <Modal
+                closeIcon
+                open={settings}
+                onOpen={() => setSettings(true)}
+                onClose={() => setSettings(false)}
+                trigger={
+                  <Menu.Item
+                      className='sidebar-item'
+                      style={{ bottom: '0' }}>
+                    <Icon name='user circle outline'/>
+                    {Meteor.user() ? Meteor.user().username : 'Guest'}
+                  </Menu.Item>
+                }
+            >
+              <Header className='card-modal'>Settings</Header>
+              <Modal.Content className='card-modal'>
+                <Settings
+                    docId={props.userProfile._id}
+                    username={props.userProfile.username}
+                    userMpg={props.userProfile.autoMPG}
+                    userHomeRoundTrip={props.userProfile.homeRoundTrip}
+                />
+              </Modal.Content>
+            </Modal>
+          </Sidebar></div>
           : '');
 };
 
