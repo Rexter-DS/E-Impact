78d21734cb70c7e8baf1bb51f1ba234ad352ddb5
Rexter Delos Santos <49216656+Rexter-DS@users.noreply.github.com>
2021-04-25 10:04:00 -1000

    Update sidebar.component.js

--- app/tests/sidebar.component.js
+++ app/tests/sidebar.component.js
@@ -8,6 +8,12 @@ class SideBar {
         this.pageSelector = Selector(this.pageId);
         await testController.expect(this.pageSelector.exists).ok();
     }
+    
+    /** Check that the specified user is currently logged in. */
+    async isLoggedIn(testController, username) {
+        const loggedInUser = Selector('#sidebar-current-user').innerText;
+        await testController.expect(loggedInUser).eql(username);
+    }
 }
 
 export const sideBar = new SideBar();
